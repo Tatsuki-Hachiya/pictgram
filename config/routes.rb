50beeb5be1e3de68bd7c60c15971d0a6a3125dfa
蜂矢 樹 <tatsuki1004h@gmail.com>
2020-11-20 08:34:33 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # get 'users/new'
   root 'pages#index'
   # get 'pages/index'
@@ -6,9 +7,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
 
 # Rails.application.routes.draw do
+  # get 'sessions/new'
 #   root 'pages#index'
 #   get 'pages/help'
 
